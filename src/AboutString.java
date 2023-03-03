fa5d03c7a7220fcb1f21cfdbfe24a244ff1bed59
nikhi <nikhil.gupta_cs19@gla.ac.in>
2023-03-03 18:29:57 +0530

    Added AboutString class and StringBuilder

---
+++ src/AboutString.java
@@ -0,0 +1,17 @@
+
+public class AboutString {
+    public static void main(String[] args) {
+        String s1="nikhil";
+        String s2=s1+" gupta";
+        String s3="nikhil";
+        StringBuilder stringBuilder=new StringBuilder("AboutString");
+        StringBuilder stringBuilder1=new StringBuilder("AboutString");
+        if(s1.equals(s3)){                         //// same reference
+            System.out.println("string");
+        }
+        if(stringBuilder.equals(stringBuilder1)){   /// they are at different references
+            System.out.println("object");
+        }
+
+    }
+}
